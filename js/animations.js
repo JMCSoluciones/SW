bea9537f1511be06241a14bbd7784723ad0d1cb2
Josué Martínez Carrasco <josue@jmcsoluciones.com>
2023-10-20 11:13:56 -0600

    Listo index en versiones Mobile, Tablet e Index

---
+++ js/animations.js
@@ -0,0 +1,17 @@
+
+	$(window).scroll(function() {
+		$('.entrance').each(function(){
+		var imagePos = $(this).offset().top;
+
+		var topOfWindow = $(window).scrollTop();
+			if (imagePos < topOfWindow+400) {
+				$(this).addClass("slideUp");
+			}
+		});
+	});
+
+
+
+	$('#animatedElement').click(function() {
+		$(this).addClass("slideUp");
+	});
